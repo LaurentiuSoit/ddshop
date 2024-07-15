cb8b34ced6924cfd0c2d44cef454a7169219155d
soitlaur <60842345+soitlaur@users.noreply.github.com>
2024-07-15 15:38:47 +0300

    Added delete product function

--- src/main/java/dd/projects/ddshop/Controllers/ProductRest.java
+++ src/main/java/dd/projects/ddshop/Controllers/ProductRest.java
@@ -5,9 +5,7 @@ import dd.projects.ddshop.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,5 +24,15 @@ public class ProductRest {
             ex.printStackTrace();
         }
         return new ResponseEntity<>(new ArrayList<ProductDTO>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) {
+        try {
+            return productService.deleteProduct(id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<String>("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
